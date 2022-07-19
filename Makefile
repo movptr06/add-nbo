f4ad41ba9892dedf2a8cc00d1108af7a545f49ab
movptr06 <movptr06@outlook.kr>
2022-07-20 08:41:47 +0900

    Add add-nbo.c

---
+++ Makefile
@@ -0,0 +1,9 @@
+#Makefile
+
+all: add-nbo
+
+add-nbo: add-nbo.c
+	gcc -o add-nbo add-nbo.c
+
+clean:
+	rm -f add-nbo
---
+++ add-nbo.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <netinet/in.h>
+
+int main(int argc, char **argv) {
+	FILE *fp;
+	uint32_t n1 = 0;
+	uint32_t n2 = 0;
+	uint32_t *p;
+	if(argc != 3) return 0;
+	fp = fopen(argv[1], "rb");
+	if(!fp) return 0;
+	p = &n1;
+	fread(p, sizeof(uint32_t), 1, fp);
+	fclose(fp);
+	fp = fopen(argv[2], "rb");
+	if(!fp) return 0;
+	p = &n2;
+	fread(p, sizeof(uint32_t), 1, fp);
+	fclose(fp);
+	n1 = ntohl(n1);
+	n2 = ntohl(n2);
+	return 0;
+}
